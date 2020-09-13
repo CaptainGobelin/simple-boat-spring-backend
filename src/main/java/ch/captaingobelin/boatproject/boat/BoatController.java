55beccc0191b31c8df1e727a368341b9b0fbfb7f
CaptainGobelin <captainGobelin@gmail.com>
2020-09-13 17:44:24 +0200

    Add boat CRUD

--- src/main/java/ch/captaingobelin/boatproject/boat/BoatController.java
+++ src/main/java/ch/captaingobelin/boatproject/boat/BoatController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +33,27 @@ public class BoatController {
 	@GetMapping(value="/boats")
 	public List<Boat> getAllBoats() {
 		return repository.findAll();
+	}
+	
+	@PostMapping(value="/add_boat")
+	public Boat addBoat(@RequestBody Boat boat) {
+		return repository.save(boat);
+	}
+	
+	@DeleteMapping(value="/{id}/delete")
+	public void removeBoat(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
+	
+	@PutMapping("/{id}/update")
+	public Boat updateBoat(@RequestBody Boat boat, @PathVariable Long id) {
+		Boat toUpdateBoat = repository.findById(id).orElse(null);
+		if (toUpdateBoat == null ) {
+			return addBoat(boat);
+		}
+		toUpdateBoat.setName(boat.getName());
+		toUpdateBoat.setDescription(boat.getDescription());
+		return repository.save(toUpdateBoat);
 	}
 	
 }
