2d158f5c13f090b9a112f3427ec611a4c77b6008
CaptainGobelin <captainGobelin@gmail.com>
2020-09-14 10:24:05 +0200

    Add sql files to initialisation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+  boats (name, description)
+VALUES
+  ('SS-Survivaure', 'Capitaine Bleuten'),
+  ('Black Pearl', 'Jack Sparow'),
+  ('Enterprise', 'Captain Kirk');
+  
+INSERT INTO
+  users (username, password)
+VALUES
+  ('user', 'password'),
+  ('user2', '$2y$10$XWaZuUbjH/3d2rpSKFs85etpAIupSS/0sRquOie21Xsk8MgLyIXiW');
+
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,16 +17,3 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 ) ;
 
-INSERT INTO
-  boats (name, description)
-VALUES
-  ('SS-Survivaure', 'Capitaine Bleuten'),
-  ('Black Pearl', 'Jack Sparow'),
-  ('Enterprise', 'Captain Kirk');
-  
-INSERT INTO
-  users (username, password)
-VALUES
-  ('user', 'password'),
-  ('user2', '$2y$10$XWaZuUbjH/3d2rpSKFs85etpAIupSS/0sRquOie21Xsk8MgLyIXiW');
-
